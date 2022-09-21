78d5fcb46a0b66a36f543f5199d49eced06ca552
deborahlea770 <deborahlea770@gmail.com>
2022-09-21 17:31:08 +0300

    fix

--- UI/conftest.py
+++ UI/conftest.py
@@ -3,4 +3,16 @@ import pytest
 
 def pytest_addoption(parser):
     parser.addoption("--url", action="store", default="http://localhost")
+    parser.addoption("--chromeDriver", action="store", default="C:/Users/debor/Downloads/chromedriver_win32/chromedriver.exe")
+
+
+@pytest.fixture
+def url(pytestconfig) -> str:
+    """
+    give the url from pytest options
+    :param pytestconfig: pytestconfig fixture
+    :return: url to integrate
+    """
+    return pytestconfig.getoption("url")
+
 
